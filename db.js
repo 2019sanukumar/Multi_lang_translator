237e6ffd55240ab64b3836a37b7761a7658c1381
Sanu Kumar <sanukumar@Sanus-MacBook-Air.local>
2021-08-24 20:18:19 +0530

    Added async await

--- db.js
+++ db.js
@@ -1,20 +1,30 @@
 const mysql=require('mysql');
 
-var con=mysql.createConnection({
-    host:"translator",
-    user:"sanu",
-    password:"9790"
+const express=require('express');
+const app=express();
+
+const bodyparser=require('body-parser');
+app.use(bodyparser.json());
+const con=mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:"password",
+  database:'trans',
+  insecureAuth : true,
+  multipleStatements:true
   
 });
+
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
   
-con.connect(function(err){
-    if(err)
-    {
-      console.log("error",err);
-      return;
-    };
-    
-      console.log("conndected to b");
-    
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
 });
+
   
--- trans.js
+++ trans.js
@@ -1,11 +1,16 @@
 const express=require('express');
 const mysql=require('mysql');
 const translate = require('@iamtraction/google-translate');
-const bodyparser=require('body-parser');
+
 const app=express();
-const port=6000;
-app.use(bodyparser);
+const port=8080;
+// const db=require('./db');
+app.use(express.json());
+
 // creating connection for mysql database
+
+
+
 const con=mysql.createConnection({
   host:'localhost',
   user:'root',
@@ -16,16 +21,108 @@ const con=mysql.createConnection({
   
 });
 
-con.connect(function(err){
-  if(err)
-  {
-    console.log("error",err);
-    return;
-  };
+
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "CREATE TABLE language (name VARCHAR(255), lang VARCHAR(255))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+// });
+
+
+// con.connect(function(err){
+//   if(err)
+//   {
+//     console.log("error",err);
+//     return;
+//   };
   
-    console.log("conndected to db");
+//     console.log("conndected to db");
   
+// });
+
+
+app.get('/',(req,res)=>
+{
+  res.send('Home page jjj');
+
+});
+let src;
+app.post('/api/from',(req,res)=>
+{
+  console.log(req.body,"req");
+  
+  // console.log(src.from);
+  res.send('Home page from');
+
+});
+app.post('/api/to',function(req,res){
+  console.log(req.body);
+  src=req.body;
+  res.send('home to');
+});
+
+
+
+let translatedtext1;
+
+app.get('/translate',  async function(req,res)
+{
+  // 
+
+
+  
+    let response = await translate('Thank you', { from: 'auto', to: 'it' });
+      if (response.err) { console.log('error');}
+      else { 
+        console.log(response.text)
+        translatedtext1=response.text;
+      };
+  
+      console.log(translatedtext1);
+
+  res.send(translatedtext1);
+
+
+  // translate('Thank you', { from: 'auto', to: src.to }).then(res => {
+  //   console.log(res.text); // OUTPUT: Je vous remercie
+  //   translatedtext1=res.text;
+  //   // out.push(translatedtext);
+  //   console.log(translatedtext1);
+  //   // console.log(res.from.autoCorrected); // OUTPUT: true
+  //   // console.log(res.from.text.value); // OUTPUT: [Thank] you
+  //   // console.log(res.from.text.didYouMean); // OUTPUT: false
+
+
+    
+    
+
+
+
+
+
+
+
+  // }).catch(err => {
+  //   console.error(err);
+  // });
+  
+  
+
+  
+});
+
+app.get('/transleted-text',function(req,res)
+{
+  // res.send.json(200)
+  res.end(JSON.stringify({ text:translatedtext1 }));
 })
+
+
 
 // app.get('/',function(req,res)
 // {
@@ -38,50 +135,60 @@ con.connect(function(err){
 // })
 
 
-  let arr=['en','hi','it'];
-  let out=[];
-  for(var i=0;i<arr.length;i++)
-  {
-      let temp=arr[i];
-      let translatedtext;
+//   let arr=['en','hi','it'];
+//   let out=[];
+//   for(var i=0;i<arr.length;i++)
+//   {
+//       let temp=arr[i];
+//       let translatedtext;
 
-      translate('Thank you', { from: 'auto', to: temp }).then(res => {
-        console.log(res.text); // OUTPUT: Je vous remercie
-        translatedtext=res.text;
-        out.push(translatedtext);
-        console.log(translatedtext);
-        // console.log(res.from.autoCorrected); // OUTPUT: true
-        // console.log(res.from.text.value); // OUTPUT: [Thank] you
-        // console.log(res.from.text.didYouMean); // OUTPUT: false
-      }).catch(err => {
-        console.error(err);
-      });
+//       translate('Thank you', { from: 'auto', to: temp }).then(res => {
+//         console.log(res.text); // OUTPUT: Je vous remercie
+//         translatedtext=res.text;
+//         out.push(translatedtext);
+//         console.log(translatedtext);
+//         // console.log(res.from.autoCorrected); // OUTPUT: true
+//         // console.log(res.from.text.value); // OUTPUT: [Thank] you
+//         // console.log(res.from.text.didYouMean); // OUTPUT: false
+
+        
+
+
+
+
+
+
+
+
+//       }).catch(err => {
+//         console.error(err);
+//       });
   
     
-}
-
-
-
-// let arr=['en','hi','it'];
-// let out=[];
-// for(var i=0;i<arr.length;i++)
-// {
-//     let temp=arr[i];
-//     let translatedtext;
-
-//     translate('Thank you', { from: 'auto', to: temp }).then(res => {
-//       console.log(res.text); // OUTPUT: Je vous remercie
-//       translatedtext=res.text;
-//       out.push(translatedtext);
-//       console.log(translatedtext);
-//       // console.log(res.from.autoCorrected); // OUTPUT: true
-//       // console.log(res.from.text.value); // OUTPUT: [Thank] you
-//       // console.log(res.from.text.didYouMean); // OUTPUT: false
-//     }).catch(err => {
-//       console.error(err);
-//     });
-    
 // }
+
+
+
+// // let arr=['en','hi','it'];
+// // let out=[];
+// // for(var i=0;i<arr.length;i++)
+// // {
+// //     let temp=arr[i];
+// //     let translatedtext;
+
+// //     translate('Thank you', { from: 'auto', to: temp }).then(res => {
+// //       console.log(res.text); // OUTPUT: Je vous remercie
+// //       translatedtext=res.text;
+// //       out.push(translatedtext);
+// //       console.log(translatedtext);
+// //       // console.log(res.from.autoCorrected); // OUTPUT: true
+// //       // console.log(res.from.text.value); // OUTPUT: [Thank] you
+// //       // console.log(res.from.text.didYouMean); // OUTPUT: false
+// //     }).catch(err => {
+// //       console.error(err);
+// //     });
+    
+// // }
 
 app.listen(port,function(){
   console.log('server is running on port 6000');
